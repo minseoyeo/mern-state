2806c1e3d9db57044e8a51b4895b9552d5bfec22
minseoyeo <minseoyeo13@gmail.com>
2024-03-04 21:11:43 +0900

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -125,6 +125,24 @@ export default function Profile() {
     } catch (error) {
       setShowListingError(true);
     }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (data.access === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListing((prev) => 
+        prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   return (
@@ -218,7 +236,7 @@ export default function Profile() {
               </Link>
   
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
