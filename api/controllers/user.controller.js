60d5798fd98fc1e8b54c3b6907296cf88a6b333b
minseoyeo <minseoyeo13@gmail.com>
2024-03-08 14:35:27 +0900

    revision

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,7 +15,7 @@ export const updateUser = async (req, res, next) => {
       req.body.password = bcryptjs.hashSync(req.body.password, 10)
     }
 
-    const updateUser = await User.findByIdAndUpdate(req.params.id, {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
       $set: {
         username: req.body.username,
         email: req.body.email,
@@ -24,7 +24,7 @@ export const updateUser = async (req, res, next) => {
       }
     }, {new: true})
 
-    const {password, ...rest} = updateUser._doc;
+    const {password, ...rest} = updatedUser._doc;
 
     res.status(200).json(rest);
   } catch (error) {
